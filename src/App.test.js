49ab596c09ddee5e9a9d6f4f0a8ef0f6803bbb81
adrija-c <146987368+adrija-c@users.noreply.github.com>
2023-10-18 21:10:37 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,6 @@
 import { render, fireEvent, screen } from '@testing-library/react';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
 
 describe("Board component", () => {
   it("renders without crashing", () => {
